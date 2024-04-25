f5531f1b91f3acea2b2f62f35fa65c800631e9f5
Ray Andus <ray.andus@ombori.com>
2024-04-26 00:34:43 +0800

    fix: baseurl

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Lusitana, Courier_Prime } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ["400", "700"] });
+
+export const courierPrime = Courier_Prime({ subsets: ['latin'], weight: ["400", "700"] });
